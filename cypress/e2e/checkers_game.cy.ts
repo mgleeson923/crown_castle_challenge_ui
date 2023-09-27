d41f83e8a6acb9ee72157f27bf8e989ffb5e8d49
mgleeson923 <mgleeson923@gmail.com>
2023-09-27 12:05:44 -0400

    Set up first test

--- cypress/e2e/checkers_game.cy.ts
+++ cypress/e2e/checkers_game.cy.ts
@@ -1,6 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('checkers game challenge', () => {
+  it('nagivates to Checkers Game Website', () => {
     cy.visit('https://www.gamesforthebrain.com/game/checkers/')
-    expect (true).to.equal(true)
+    cy.get('[id="message"]').invoke('text').then((text) => {
+      cy.log(text)
+    
+      expect(text).to.equal("Select an orange piece to move.")
+    })
   })
 })
