61aea013b3d4720d9d5957f66086f9eab3836eb3
mgleeson923 <mgleeson923@gmail.com>
2023-09-27 12:50:54 -0400

    Updated ReadMe Added additional test steps.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # crown_castle_challenge_ui
 UI Challenge for The Checkers Game
+
+Notes:
+ - Because the opponent follows your move with the same one each time, it was simpler to just establish a pattern. Ideally, we'd want to make this test more modular and select pieces at random, then analyze where the bot moved. 
+ - Secondly, the elements should have some sort of automation tag (ex: data-qa="orange piece 1") or something of that nature. That would make it easier to follow the test than having to use the space names. 
+ - With extra time, I'd want to set up a link checker that runs through all the links in the footer and verifies they're healthy links (ie: just confirm a 200 code if they're clicked, and that they route to the expected locations)
+ - I'd also look at some negative scenarios (trying to move a piece to an ineligible board spot and verifying the message displays the correct warning, etc)
--- cypress/e2e/checkers_game.cy.ts
+++ cypress/e2e/checkers_game.cy.ts
@@ -1,10 +1,53 @@
 describe('checkers game challenge', () => {
   it('nagivates to Checkers Game Website', () => {
     cy.visit('https://www.gamesforthebrain.com/game/checkers/')
-    cy.get('[id="message"]').invoke('text').then((text) => {
-      cy.log(text)
-    
-      expect(text).to.equal("Select an orange piece to move.")
-    })
+    cy.get('[id="message"]').should('have.text', 'Select an orange piece to move.')
   })
+
+  it('makes the first move', () => {
+    cy.get('[name="space22"]').click()
+    cy.get('[name="space33"]').click()
+    cy.get('[id="message"]').should('have.text', 'Make a move.')
+    cy.wait(2000)
+  })
+
+
+  it('makes the second move', () => {
+    cy.get('[name="space42"]').click()
+    cy.get('[name="space53"]').click()
+    cy.get('[id="message"]').should('have.text', 'Make a move.')
+    cy.wait(2000)
+
+  })
+
+  it('makes the third move', () => {
+    cy.get('[name="space62"]').click()
+    cy.get('[name="space73"]').click()
+    cy.get('[id="message"]').should('have.text', 'Make a move.')
+    cy.wait(2000)
+
+  })
+
+  it('makes the fourth move and captures a blue piece', () => {
+    cy.get('[name="space31"]').click()
+    cy.get('[name="space53"]').click()
+    cy.get('[id="message"]').should('have.text', 'Make a move.')
+    cy.wait(2000)
+
+  })
+
+  it('makes the fifth move', () => {
+    cy.get('[name="space51"]').click()
+    cy.get('[name="space42"]').click()
+    cy.get('[id="message"]').should('have.text', 'Make a move.')
+    cy.wait(2000)
+
+  })
+
+  it('restarts the game', () => {
+    cy.contains("Restart").click()
+    cy.get('[id="message"]').should('have.text', 'Select an orange piece to move.')
+
+  })
+
 })
